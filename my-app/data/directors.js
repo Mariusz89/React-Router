68a0dec17eeae51bcc4bbf07c57a6eaf90f2c301
Mariusz <mariusz.kkk@poczta.fm>
2018-09-15 22:44:19 +0200

    Add dates directors/films

---
+++ my-app/data/directors.js
@@ -0,0 +1,40 @@
+const DirectorList = [
+  {
+    name: "Quentin Tarantino",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis volutpat elementum. Vestibulum quis tempus purus, a fermentum mauris. Donec ornare ac purus sit amet interdum.",
+    img_src: "",
+    id: "director-1"
+  },
+  {
+    name: "Martin Scorsese",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis volutpat elementum. Vestibulum quis tempus purus, a fermentum mauris. Donec ornare ac purus sit amet interdum.",
+    img_src: "",
+    id: "director-2"
+  },
+  {
+    name: "Christopher Nolan I",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis volutpat elementum. Vestibulum quis tempus purus, a fermentum mauris. Donec ornare ac purus sit amet interdum.",
+    img_src: "",
+    id: "director-3"
+  },
+  {
+    name: "Lilly Wachowski",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis volutpat elementum. Vestibulum quis tempus purus, a fermentum mauris. Donec ornare ac purus sit amet interdum.",
+    img_src: "",
+    id: "director-4"
+  },
+  {
+    name: "Tim Burton",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis volutpat elementum. Vestibulum quis tempus purus, a fermentum mauris. Donec ornare ac purus sit amet interdum.",
+    img_src: "",
+    id: "director-5"
+  },
+  {
+    name: "James Wan I",
+    bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis volutpat elementum. Vestibulum quis tempus purus, a fermentum mauris. Donec ornare ac purus sit amet interdum.",
+    img_src: "",
+    id: "director-6"
+  }
+];
+
+export default DirectorList;
---
+++ my-app/data/films.js
@@ -0,0 +1,116 @@
+export const Thrillers = [
+  {
+    title: "Django",
+    description: "Łowca nagród Schultz i czarnoskóry niewolnik Django wyruszają w podróż, aby odbić żonę tego drugiego z rąk bezlitosnego Calvina Candie'ego.",
+    img_src:"",
+    id: "thrillers-1"
+  },
+  {
+    title: "Kill Bill",
+    description: "Płatna zabójczyni zwana Czarną Mambą po czterech latach wybudza się ze śpiączki z zamiarem krwawej zemsty na ludziach, którzy próbowali ją zabić.",
+    img_src: "",
+    id: "thrillers-2"
+  },
+  {
+    title: "Wściekłe psy",
+    description: "Po nieudanym napadzie na sklep jubilerski kilku złodziei zdaje sobie sprawę, że jest wśród nich zdrajca, który powiedział policji o ich planie. ",
+    img_src: "",
+    id: "thrillers-3"
+  },
+  {
+    title: "Wyspa tajemnic",
+    description: "Szeryf federalny Daniels stara się dowiedzieć, jakim sposobem z zamkniętej celi w pilnie strzeżonym szpitalu dla chorych psychicznie przestępców zniknęła pacjentka.",
+    img_src: "",
+    id: "thrillers-4"
+  },
+  {
+    title: "Kasyno",
+    description: "Las Vegas, rok 1973. Hazardzista i bukmacher, Sam 'Ace' Rothstein, na polecenie mafii zostaje szefem jednego z największych kasyn w mieście.",
+    img_src: "",
+    id: "thrillers-5"
+  },
+  {
+    title: "Taksówkarz",
+    description: "Weteran wojny w Wietnamie zatrudnia się jako taksówkarz. Jeżdżąc wieczorami po nowojorskich ulicach, dochodzi do wniosku, że musi rozprawić się z 'robactwem' tego miasta.",
+    img_src: "",
+    id: "thrillers-6"
+  }
+  ];
+  
+export const Sci_Fi =[
+    {
+      title: "Mroczny Rycerz",
+      description: "Batman, z pomocą porucznika Gordona oraz prokuratora Harveya Denta, występuje przeciwko przerażającemu i nieobliczalnemu Jokerowi, który chce pogrążyć Gotham City w chaosie.",
+      img_src: "",
+      id: "sci_Fi-1"
+    },
+    {
+      title: "Incepcja",
+      description: "Czasy, gdy technologia pozwala na wchodzenie w świat snów. Złodziej Cobb ma za zadanie wszczepić myśl do śpiącego umysłu.",
+      img_src: "",
+      id: "sci_Fi-2"
+    },
+    {
+      title: "Interstellar",
+      description: "Byt ludzkości na Ziemi dobiega końca wskutek zmian klimatycznych. Grupa naukowców odkrywa tunel czasoprzestrzenny, który umożliwia poszukiwanie nowego domu.",
+      img_src: "",
+      id: "sci_Fi-3"
+    },
+    {
+      title: "Matrix",
+      description: "Haker komputerowy Neo dowiaduje się od tajemniczych rebeliantów, że świat, w którym żyje, jest tylko obrazem przesyłanym do jego mózgu przez roboty.",
+      img_src: "",
+      id: "sci_Fi-4"
+    },
+    {
+      title: "V jak Vendetta",
+      description: "W totalitarnej Anglii jedyną osobą walczącą o wolność jest bojownik przebrany za Guya Fawkesa – V, który pewnego dnia uwalnia z rąk agentów rządowych młodą kobietę.",
+      img_src: "",
+      id: "sci_Fi-5"
+    },
+    {
+      title: "Atlas chmur",
+      description: "Rozgrywająca się w różnych epokach czasowych i zakątkach świata opowieść o losach ludzi, których czyny mają wpływ na przeszłość, teraźniejszość oraz przyszłość.",
+      img_src: "",
+      id: "sci_Fi-6"
+    }
+  ];
+  
+export const Horrors = [
+  {
+    title:"Planeta małp",
+    description:"Astronauta trafia na planetę, gdzie władzę sprawują inteligentne małpy. Wkrótce mężczyzna przyłącza się do buntowników walczących o wolność.",
+    img_src:"",
+    id: "horrors-1"
+  },
+  {
+    title:"Jeździec bez głowy",
+    description:"Nowojorski policjant przybywa do małej osady, by rozwiązać sprawę tajemniczych morderstw dokonywanych rzekomo przez jeźdźca bez głowy.",
+    img_src:"",
+    id: "horrors--2"
+  },
+  {
+    title: "Mroczne cienie",
+    description: "Ukarany przez mściwą wiedźmę Barnabas wstaje z grobu po dwustu latach i trafia w szalone lata 70., których zupełnie nie rozumie.",
+    img_src: "",
+    id: "horrors-3"
+  },
+  {
+    title: "Obecność",
+    description: "Słynna para badaczy zjawisk paranormalnych zostaje poproszona o pomoc rodzinie, którą terroryzuje potężny demon.",
+    img_src: "",
+    id: "horrors-4"
+  },
+  {
+    title: "Piła",
+    description: "Dwóch mężczyzn budzi się przykutych do zardzewiałej rury w piwnicy. Zdają sobie sprawę, że są uczestnikami krwawej gry szaleńca.",
+    img_src: "",
+    id: "horrors-5"
+  },
+  {
+    title: "Naznaczony",
+    description: "Po upadku z drabiny chłopiec traci przytomność. Będąc w śpiączce, wchodzi do świata umarłych.",
+    img_src: "",
+    id: "horrors-6"
+  }
+];
