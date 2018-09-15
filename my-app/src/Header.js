ddcfd0fb3fdf77b08677e287d848251f13204167
Mariusz <mariusz.kkk@poczta.fm>
2018-09-16 00:47:12 +0200

    Add component Header nav

---
+++ my-app/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => (
+	<header>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Directors</a></li>
+			<li><a href="#">Films</a></li>
+		</ul>
+	</header>
+);
+
+export default Header;
