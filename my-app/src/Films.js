0ebae68252450af159421ee93ff975d29f329c95
Mariusz <mariusz.kkk@poczta.fm>
2018-09-16 01:46:54 +0200

    Add component Films

---
+++ my-app/src/Films.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Films = () => (
+	<div>
+		<h2>Films</h2>
+		<ul>
+			<li><a href='/films/thriller'>Thriller</a></li>
+			<li><a href='/films/sci-fi'></a>Sci-Fi</li>
+			<li><a href='/films/horror'></a>Horror</li>
+		</ul>
+	</div>
+);
+
+export default Films;
