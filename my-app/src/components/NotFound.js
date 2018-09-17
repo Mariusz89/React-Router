1b5484c702f1d76d1d96d198a657424ec1aab7ff
Mariusz <mariusz.kkk@poczta.fm>
2018-09-18 01:53:04 +0200

    Change name class to fa-exclamation; Add <Switch> that renders the first child <Route> or <Redirect> that matches the location

--- my-app/src/components/NotFound.js
+++ my-app/src/components/NotFound.js
@@ -3,7 +3,7 @@ import 'font-awesome/css/font-awesome.min.css';
 
 const NotFound = () => (
 	<div className="main-content not-found">
-		<i className="fa fa-exclamation fa-5x"></i>
+		<i className="fa fa-exclamation"></i>
 		<h2>PAGE NOT FOUND</h2>
 	</div>
 );
